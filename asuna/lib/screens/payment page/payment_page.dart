c3d683f727be7781d9178e7c0a47925956d6b4ea
Yusufxon0909 <yusufxon0909@gmail.com>
2024-09-11 15:05:06 +0500

    7-qadam

--- asuna/lib/screens/payment page/payment_page.dart
+++ asuna/lib/screens/payment page/payment_page.dart
@@ -10,12 +10,18 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  List<Simple> name = List.generate(
-    18,
-    (index) => Simple(
-      name: nameindex(index),
-    ),
-  );
+  List<Simple> name = [];
+
+  @override
+  void initState() {
+    super.initState();
+    name = List.generate(
+      18,
+      (index) => Simple(
+        name: nameindex(index),
+      ),
+    );
+  }
 
   static String nameindex(int index) {
     switch (index) {
@@ -64,11 +70,11 @@ class _PaymentPageState extends State<PaymentPage> {
     switch (index) {
       case 0:
         Navigator.push(
-            context, MaterialPageRoute(builder: (context) => case0()));
+            context, MaterialPageRoute(builder: (context) => MobilePage())); // case0() to MobilePage
         break;
       case 1:
         Navigator.push(
-            context, MaterialPageRoute(builder: (context) => case1()));
+            context, MaterialPageRoute(builder: (context) => DavlatXizmatlariPage())); // case1() to DavlatXizmatlariPage
         break;
       // Add cases for other pages as needed
       default:
@@ -83,17 +89,6 @@ class _PaymentPageState extends State<PaymentPage> {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: SingleChildScrollView(
-<<<<<<< HEAD
-        child: Column(children: [
-          const Padding(
-            padding: EdgeInsets.all(15),
-            child: Text(
-              "To'lov",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500),
-=======
         child: Center(
           child: Column(
             children: [
@@ -163,7 +158,7 @@ class _PaymentPageState extends State<PaymentPage> {
                       child: InkWell(
                         onTap: () {
                           Navigator.push(context,
-                              MaterialPageRoute(builder: (context) => case0()));
+                              MaterialPageRoute(builder: (context) => MobilePage()));
                         },
                         child: Container(
                           height: 140,
@@ -201,8 +196,7 @@ class _PaymentPageState extends State<PaymentPage> {
                           Navigator.push(
                               context,
                               MaterialPageRoute(
-                                  builder: (context) =>
-                                      case0())); // Change this to appropriate page
+                                  builder: (context) => MobilePage())); // Change this to appropriate page
                         },
                         child: Container(
                           height: 140,
@@ -298,7 +292,7 @@ class _PaymentPageState extends State<PaymentPage> {
           onTap: () => _navigateToPage(index, context),
           child: Container(
             height: 80,
-            width: double.infinity, // Use double.infinity to expand the width
+            width: double.infinity,
             color: Color.fromARGB(221, 57, 57, 57),
             child: Padding(
               padding: const EdgeInsets.only(top: 25, left: 18),
@@ -310,209 +304,15 @@ class _PaymentPageState extends State<PaymentPage> {
                   fontWeight: FontWeight.w500,
                 ),
               ),
->>>>>>> origin/payment_page_branch
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xFF2C2C2E), // Dark background color for the search bar
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Icon(
-                        Icons.search,
-                        color: Color(0xFFA0A0A0),
-                        size: 30, // Color for the search icon
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(4.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Qidiruv', // Search hint text
-                          hintStyle: TextStyle(color: Color(0xFFA0A0A0), fontSize: 20),
-                          border: InputBorder.none,
-                        ),
-                        style: TextStyle(color: Colors.white), // Text color
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15, top: 8, bottom: 8),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const TheBottomNavigationBar()));
-                      },
-                      child: const Icon(
-                        Icons.qr_code_scanner_outlined, // QR code icon
-                        color: Color(0xFF007AFF),
-                        size: 35, // Color for the QR code icon
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30, left: 10, right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const TheBottomNavigationBar()));
-                  },
-                  child: Container(
-                    height: 140,
-                    width: 140,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color.fromARGB(221, 57, 57, 57)),
-                    child: const Padding(
-                      padding: EdgeInsets.only(left: 10, top: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.star_border,
-                            size: 35,
-                            color: Colors.blue,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Sraralangan to'lovlar",
-                            style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const TheBottomNavigationBar()));
-                  },
-                  child: Container(
-                    height: 140,
-                    width: 140,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color.fromARGB(221, 57, 57, 57)),
-                    child: const Padding(
-                      padding: EdgeInsets.only(left: 10, top: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.calendar_month_outlined,
-                            size: 35,
-                            color: Colors.blue,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            " Avtoto'lovlar",
-                            style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                const TheBottomNavigationBar()));
-                  },
-                  child: Container(
-                    height: 140,
-                    width: 140,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: const Color.fromARGB(221, 57, 57, 57)),
-                    child: const Padding(
-                      padding: EdgeInsets.only(left: 10, top: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.add_location_alt_outlined,
-                            size: 35,
-                            color: Colors.blue,
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            "Joylarda to'lova",
-                            style: TextStyle(
-                                fontSize: 17,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
-            child: Column(
-              children: name.map((ism2) => rock(ism2, context)).toList(),
-            ),
-          ),
-        ]),
+        ),
       ),
     );
   }
 }
 
-<<<<<<< HEAD
 class Simple {
-=======
-// ignore: camel_case_types
-class simple {
->>>>>>> origin/home_page_branch
   IconData? simble;
   String? ism;
   String? info;
@@ -535,38 +335,23 @@ class simple {
     this.down,
   });
 }
-<<<<<<< HEAD
 
-Widget rock(Simple ism2, BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.all(5.0),
-    child: ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const TheBottomNavigationBar()));
-        },
-        child: Container(
-          height: 80,
-          width: 430,
-          color: const Color.fromARGB(221, 57, 57, 57),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 25, left: 18),
-            child: Text(
-              ism2.name ?? '',
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+class MobilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Mobile Page')),
+      body: Center(child: Text('Mobile Page Content')),
+    );
+  }
 }
-=======
->>>>>>> origin/payment_page_branch
+
+class DavlatXizmatlariPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Davlat Xizmatlari Page')),
+      body: Center(child: Text('Davlat Xizmatlari Page Content')),
+    );
+  }
+}
